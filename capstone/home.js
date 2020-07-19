8be2489d8e8550ac60e9b5f4cf12fb94606caa54
Kara <kara.manseau@getg5.com>
2020-07-19 11:44:17 -0700

    SQL api functions for delete functionality

--- capstone/home.js
+++ capstone/home.js
@@ -37,42 +37,39 @@ module.exports = function(){
 			});
 	});
 	router.get('/delete_employee', function(req,res){
-
+		let mysql = req.app.get('mysql');
 		console.log("delete employee: ", req.query.id);
-		res.redirect('/');
-		// res.redirect("/");
-		// mysql.pool.query('SELECT e.*, employee.* FROM `employer_employee` as e LEFT JOIN employee on employee.employee_id=e.employee_id WHERE employer_id=?',[req.session.employer_id],
-		// 	function (error, employees, fields) {
-		// 		// log query results
-		// 		console.log("Employee List Results\n", employees);
-		// 		if(error){
-		// 			res.write(JSON.stringify(error));
-		// 			res.end();
-		// 		} else {
-		// 			mysql.pool.query('SELECT * FROM `quiz` WHERE employer_id=?',[req.session.employer_id],
-		// 				function (error, quizzes, fields) {
-		// 					// log query results
-		// 					console.log("quizzes List Results\n", quizzes);
-		// 					if(error){
-		// 						res.write(JSON.stringify(error));
-		// 						res.end();
-		// 					} else {
-		// 						res.render('home', {
-		// 							full_name:req.session.employer_name,
-		// 							employee: employees,
-		// 							quiz: quizzes
-		// 						});
-		// 					}
-		// 				});
-		// 		}
-		// 	});
+		mysql.pool.query('DELETE FROM `employee` WHERE employee_id=?',[req.query.id],
+			function (error, result, fields) {
+				// log query results
+				console.log("Result: \n", result);
+				if (error) {
+					res.write(JSON.stringify(error));
+					res.end();
+				} else {
+					res.redirect('/');
+				}
+			});
+		// res.redirect('/');
+
+
 	});
 	router.get('/delete_quiz', function(req,res) {
 
-		// let mysql = req.app.get('mysql');
-		console.log("delete quiz: ", req.query.id);
-		res.redirect('/');
 
+		let mysql = req.app.get('mysql');
+		console.log("delete quiz: ", req.query.id);
+		mysql.pool.query('DELETE FROM `quiz` WHERE quiz_id=?',[req.query.id],
+			function (error, result, fields) {
+				// log query results
+				console.log("Result: \n", result);
+				if (error) {
+					res.write(JSON.stringify(error));
+					res.end();
+				} else {
+					res.redirect('/');
+				}
+			});
 	});
 	return router;
 }();
