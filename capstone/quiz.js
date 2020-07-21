125c4cabd22fe42113b083fe2893a85b868f3086
Brian-Dong <49142531+Brian-Dong@users.noreply.github.com>
2020-07-20 19:40:19 -0700

    Add files via upload

--- capstone/quiz.js
+++ capstone/quiz.js
@@ -56,7 +56,10 @@ module.exports = function(){
 						questions.push(question_answer_obj);
 					}
 
-					answers_arr.push(results[i].answer_text);
+					var temp_arr = [];
+					temp_arr.push(results[i].answer_text);
+					temp_arr.push(results[i].answer_id);
+					answers_arr.push(temp_arr);
 
 				}
 
@@ -67,10 +70,9 @@ module.exports = function(){
 				context.questions = questions;
 				context.quiz_name = req.session.quiz_name;
 				context.timer = req.session.timer;
+
 				res.render('quiz', context);
 			    
-
-
             }
 
 		});
@@ -83,12 +85,60 @@ module.exports = function(){
 		var session = req.app.get('session');
   		
   		console.log("body", req.body);
-  		const keys = Object.keys(req.body);
-
+  		const user_answers = Object.keys(req.body);
+  		console.log(user_answers);
   		context.quiz_finished = true;
 
+  		var query = 'SELECT * FROM (SELECT question_id, question FROM questions WHERE quiz_id=?) AS t1 INNER JOIN answers ON answers.question_id = t1.question_id WHERE answers.correct = 1';
+  		mysql.pool.query(query, [req.session.quiz_id],
+			function (error, results, fields) {
+			// log query results
+			console.log("Results\n", results);
 
-  		res.render('login', context);
+			if(error){
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+
+			else {
+
+				var itr = 0;
+				var correctCount = 0;
+				query = 'INSERT INTO results (quiz_id, question_id, employee_id, correct_question_id, selected_question_id) VALUES ?';
+				var params = [];
+				var quiz_id = req.session.quiz_id;
+				var employee_id = req.session.employee_id;
+
+				for (var i = 0; i < user_answers.length; i++){
+					for (var j = 0; j < results.length; j++){
+						if (parseInt(user_answers[i]) === results[j].question_id){
+							var question_id = results[j].question_id;
+							var correct_answer_id = results[j].answer_id;
+							var user_selection = req.body[user_answers[i]];
+
+							params.push([String(quiz_id), String(question_id),String(employee_id), String(correct_answer_id), String(user_selection)]);
+						}
+					}
+					
+				}
+				console.log("params", params);
+
+				mysql.pool.query(query, [params], function(error,results,fields){
+					if (error){
+						res.write(JSON.stringify(error));
+						res.end();
+					}
+					else {
+						req.session.destroy();
+  						res.render('login', context);
+					}
+				});
+
+
+			    
+            }
+
+        });
 
 	});
 	
